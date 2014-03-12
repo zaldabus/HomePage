a8d840ac2d8a4cee872d8c93e99902a781d310e0
Ryan Beckman <beckman.ryan@gmail.com>
2014-03-12 13:24:09 -0400

    tictactoe renders its own layout

--- app/controllers/roots_controller.rb
+++ app/controllers/roots_controller.rb
@@ -2,4 +2,8 @@ class RootsController < ApplicationController
   def index
 
   end
+
+  def tictactoe
+    render layout: nil
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Ryanbeckman::Application.routes.draw do
   root to: "roots#index"
   resources :roots, only: [:index, :create]
+  get "tictactoe", to: "roots#tictactoe"
 end
