23c9b1ac96f215ac95ed4632f8a1d15348c1d868
Ryan Beckman <beckman.ryan@gmail.com>
2014-03-17 15:19:49 -0400

    project box shadow styling, tictactoe jquery bug fixed

--- app/assets/javascripts/newGame.js
+++ app/assets/javascripts/newGame.js
@@ -3,8 +3,8 @@
   var TTT = root.TTT = (root.TTT || {});
 
   $(document).ready(function() {
-    var myGame = new root.TTT.Game($('ul'));
-     $('li').each(function(index, li){
+    var myGame = new root.TTT.Game($('.group'));
+     $('.group > li').each(function(index, li){
         $(li).on("click", myGame.makeMove.bind(myGame, $(li)));
      })
 
